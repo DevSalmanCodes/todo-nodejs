f1f5e97f4837a2038c4d265c9f03956c079218b7
DevSalmanCodes <salmanahma9990@gmail.com>
2025-01-27 09:19:24 +0500

    updated

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,26 +1,30 @@
 import jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
-import { ApiError } from "../utils/ApiError.js";
 
-async function isAuthorizedUser(req, _, next)  {
+async function isAuthorizedUser(req, res, next) {
   try {
-    const token = req.headers.authorization.split(" ")[1];
+    const token = req.headers.authorization?.split(" ")[1]; 
     if (!token) {
-      throw new ApiError(400, "Invalid token");
+      return res.status(401).json({ error: "Token is missing" });
     }
 
     const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-    console.log(decodedToken);
-    
-    if (!decodedToken) {
-      throw new ApiError(400, "Unautorized user");
+
+    const user = await User.findById(decodedToken?.id).select("-password -refreshToken");
+    if (!user) {
+      return res.status(401).json({ error: "Unauthorized user" });
     }
-    const user = await User.findById(decodedToken.id)
     req.user = user;
     next();
   } catch (err) {
-   throw new ApiError(400,err);
+    if (err.name === "TokenExpiredError") {
+      return res.status(401).json({ error: "Token has expired" });
+    } else if (err.name === "JsonWebTokenError") {
+      return res.status(401).json({ error: "Invalid token" });
+    } else {
+      return res.status(500).json({ error: "Something went wrong" });
+    }
   }
-};
+}
 
 export default isAuthorizedUser;
