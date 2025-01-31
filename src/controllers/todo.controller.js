bf009f1c50038595e493448b0509b972bdf1bf06
DevSalmanCodes <salmanahma9990@gmail.com>
2025-01-31 09:30:49 +0500

    Added add-todo and get-todos

--- src/controllers/todo.controller.js
+++ src/controllers/todo.controller.js
@@ -1,6 +1,40 @@
-async function addTodo(req, res) {}
+import Todo from "../models/todo.model.js";
 
-async function getTodos(req, res) {}
+import ApiError from "../utils/ApiError.js";
+import ApiResponse from "../utils/ApiResponse.js";
+
+async function addTodo(req, res) {
+  const { title, description } = req.body;
+  if (!title) {
+    return res.status(400).json(new ApiError(400, "Please provide title"));
+  }
+  try {
+    const todo = await Todo.create({
+      title: title,
+      description: description,
+      userId: req.user._id,
+    });
+
+    return res
+      .status(201)
+      .json(new ApiResponse(201, "Todo created successfully", todo));
+  } catch (err) {
+    return res
+      .status(500)
+      .json(new ApiError(500, err?.message || "Internal server error"));
+  }
+}
+
+async function getTodos(req, res) {
+  try {
+    const todos = await Todo.find({ userId: req.user._id });
+    return res.status(200).json(new ApiResponse(200, "Todos fetched", todos));
+  } catch (err) {
+    return res
+      .status(500)
+      .json(new ApiError(500), err?.message || "Internal server error");
+  }
+}
 
 async function updateTodo(req, res) {}
 
