71f9013bdc776fa417cc435d37588681ff6d488f
DevSalmanCodes <salmanahma9990@gmail.com>
2025-01-31 10:04:04 +0500

    Added updated-todo

--- src/routes/todo.route.js
+++ src/routes/todo.route.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { addTodo, getTodos } from "../controllers/todo.controller.js";
+import { addTodo, getTodos, updateTodo } from "../controllers/todo.controller.js";
 const router = express.Router();
 
 router.route("/add-todo").post(addTodo);
 router.route("/get-todos").get(getTodos);
+router.route("/update-todo/:id").put(updateTodo);
 export default router;
